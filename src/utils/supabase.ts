9a2df17a0d0c859213883a4e4c561498b6fb3528
Ezequiel Saizar <esaizar88@gmail.com>
2026-03-09 14:16:07 -0300

    uso variables de entorno.

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = `https://ytoozusutyyxivixtmgl.supabase.co`;
-const supabaseAnonKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0b296dXN1dHl5eGl2aXh0bWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAwMjYxNDYsImV4cCI6MjAzNTYwMjE0Nn0.zL4f2PUk_bJblL6n98Ot3S4u0ZDgVxuy6n5XlvBjaaw`;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
